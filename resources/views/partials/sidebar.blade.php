64e44875846eb7dd1049421c225d0deb3866f2d3
Mr.Programmer <m.mohammadi.developer@gmail.com>
2021-05-31 11:19:43 +0430

    fix sidebar bug

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -26,7 +26,7 @@
             @foreach (App\Models\ChecklistGroup::with('checklists')->get() as $group)
 
                 <li class="c-sidebar-nav-item c-sidebar-nav-dropdown c-show">
-                    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="{{ route('admin.checklist_groups.edit', $group->id) }}">
+                    <a class="c-sidebar-nav-link" href="{{ route('admin.checklist_groups.edit', $group->id) }}">
                         <svg class="c-sidebar-nav-icon">
                             <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
                         </svg> {{ $group->name }}
